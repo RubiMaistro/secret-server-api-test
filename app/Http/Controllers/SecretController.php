45810b3fe9defd8cc4bfd39e089d65a9eaf8f424
Deák Ruben <rubendeak15@gmail.com>
2021-11-16 21:10:19 +0100

    POST request processed

--- app/Http/Controllers/SecretController.php
+++ app/Http/Controllers/SecretController.php
@@ -5,11 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Secret;
 use Exception;
+use DateTime;
 
 class SecretController extends Controller
 {
-    public function post(Secret $secret){
-        
+    public function index(Request $request){
+        try{
+            $secret = new Secret();
+
+            $secret->hash = $this->generateRandomHash();
+            $secret->secretText = $request->get('secretText');
+            $secret->createdAt = date("Y-m-d H:i:s");
+            $secret->expiresAt = $this->getExpires($request->get('expireAfter'));
+            $secret->remainingViews = $request->get('expireAfterViews');
+            $secret->save();
+
+            return response()->json(['description' => 'Successful operation'], 200);
+
+        } catch(Exception $e) {
+            return response()->json([
+                'description' => 'Invalid input',
+                'error' => $e,
+            ], 200);
+        }
     }
 
     public function show($hash){
@@ -20,5 +38,40 @@ class SecretController extends Controller
         } 
 
         return response()->json(Secret::find($hash), 200);
+    }
+
+    private function getExpires($minutes) {
+        $dateTime = new DateTime();
+        $dateTime->format("Y-m-d H:i:s");
+        $dateTime->modify("+{$minutes} minutes");
+
+        return $dateTime;
+    }
+
+    private function generateRandomHash($length = 6) {
+
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+        $charactersLength = strlen($characters);
+        $hash = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $hash .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        if($this->isExisted($hash)) {
+            $this->generateRandomHash();
+        } else {
+            return $hash;
+        }
+    }
+
+    private function isExisted($hash) {
+       $secret = Secret::find($hash);
+
+       if(is_null($secret)) {
+           return False;
+       } else {
+           return True;
+       }
     }
 }
--- app/Models/Secret.php
+++ app/Models/Secret.php
@@ -7,9 +7,51 @@ use Illuminate\Database\Eloquent\Model;
 
 class Secret extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'secret';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
     protected $primaryKey = 'hash';
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * Indicates if the created at and updated at column are added to the table.
+     * 
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
     protected $fillable = [
-        'hash', 'secretText', 'createdAt', 'expiresAt', 'remainingViews'
+        'hash',
+        'secretText',
+        'createdAt',
+        'expiresAt',
+        'remainingViews',
     ];
 }
