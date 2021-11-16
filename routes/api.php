b71caefe9bd6d013326cb25aeb1545b9d4bae84e
Deák Ruben <rubendeak15@gmail.com>
2021-11-16 10:07:08 +0100

    Create routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/secret', [App\Http\Controllers\SecretController::class, 'index']);
+Route::get('/secret/{hash}', [App\Http\Controllers\SecretController::class, 'show']);
+
+
